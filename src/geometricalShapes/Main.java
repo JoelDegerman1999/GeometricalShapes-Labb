267772e4bdde45394c5d3fa05b4351552be3e26e
Joel <Joel@DESKTOP-5R33SO3>
2019-08-30 09:23:32 +0200

    Första commit.

---
+++ src/geometricalShapes/Main.java
@@ -0,0 +1,15 @@
+package geometricalShapes;
+
+import model.Cirkel;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Cirkel cirkel = new Cirkel(5);
+        
+        
+                
+    }
+
+}
